a42eab28df96491c2d40ea86bac6dcc7c0aff729
uu 风林 <634086003@qq.com>
2019-06-29 23:36:25 +0800

    Revert "第二次"
    This reverts commit dec8070e91384f688db1ba71f75926a5a105a0ef.

--- PythonApplication2/cookies.py
+++
@@ -1,9 +0,0 @@
-import http.cookiejar, urllib.request
-filename = 'cookies1.txt'
-cookie = http.cookiejar.MozillaCookieJar(filename)
-handler = urllib.request.HTTPCookieProcessor(cookie)
-opener = urllib.request.build_opener(handler)
-response = opener.open('http://111.198.29.45:52703')
-cookie.save(ignore_discard=True, ignore_expires=True)
- 
-
--- PythonApplication2/表单提交得到disabled.py
+++
@@ -1,6 +0,0 @@
-import urllib.parse
-import urllib.request
-data = bytes(urllib.parse.urlencode({'auth':'flag'}), encoding=
-'UTF-8')
-response = urllib.request.urlopen('http://111.198.29.45:51052/', data=data)
-print(response.read().decode())
